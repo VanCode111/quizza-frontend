d2204cbfadaa8a685c4f125c9310fb656d9b2b75
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-05-11 18:44:14 +0300

    component button is created

---
+++ components/Button.vue
@@ -0,0 +1,53 @@
+<template>
+    <button class="btn" :class="computedClasses"><slot></slot></button>
+</template>
+
+<script>
+export default {
+    props: {
+        appearance: {
+            type: String,
+            default: "error",
+        },
+    },
+    computed: {
+        computedClasses() {
+            return [`btn_appearance_${this.appearance}`];
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+.btn {
+    padding: 16px 49px;
+    border-radius: 100px;
+    border: none;
+    border-bottom: 6px solid;
+    font-size: 20px;
+    font-weight: bold;
+    cursor: pointer;
+    background-color: #ea425b;
+    color: #fff;
+    border-bottom-color: #b5283e;
+    &_appearance {
+        &_orange {
+            background-color: #f4802e;
+            border-bottom-color: rgba(181, 40, 62, 0.5);
+        }
+        &_blank {
+            background-color: #fff8e2;
+            color: #00214e;
+            border-bottom-color: #ffbd00;
+        }
+        &_selected {
+            background-color: #ffbd00;
+            border-bottom-color: #c68754;
+        }
+        &_success {
+            background-color: #28d239;
+            border-bottom-color: #1d9c2a;
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,45 @@
 <template>
-  <div>
-    <p>
-      <NuxtLink to="/Settings">жжжж гыгыгы</NuxtLink>
-    </p>
+    <div>
+        <p>
+            <NuxtLink to="/Settings">жжжж гыгыгы</NuxtLink>
+        </p>
 
-    <QuantityCounter v-model:rounds="rounds"></QuantityCounter>
-  </div>
+        <QuantityCounter v-model:rounds="rounds"></QuantityCounter>
+
+        <div class="buttons">
+            <h3>Кнопки</h3>
+            <div class="buttons__wrapper">
+                <Button>Кнопка</Button>
+                <Button appearance="orange">Кнопка</Button>
+                <Button appearance="blank">Кнопка</Button>
+                <Button appearance="selected">Кнопка</Button>
+                <Button appearance="success">Кнопка</Button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      rounds: 15,
-    };
-  },
+    data() {
+        return {
+            rounds: 15,
+        };
+    },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.buttons {
+    padding: 10px;
+    & h3 {
+        margin-bottom: 10px;
+    }
+    &__wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        align-items: start;
+    }
+}
+</style>
