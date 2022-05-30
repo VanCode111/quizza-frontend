0f9c984ea615dbc5e8e8068dc329295a52e62b61
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-05-30 20:36:18 +0300

    avatar

---
+++ components/Avatar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="avatar">
+        <img class="img" :class="computedClasses" :src="src" alt="avatar" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        src: String,
+    },
+    computed: {
+        computedClasses() {
+            const notFound = !file_exists(src) || !src;
+            return [
+                {
+                    notFound,
+                },
+            ];
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+.avatar {
+    border-radius: 50%;
+    width: 163px;
+    height: 163px;
+    overflow: hidden;
+    & .img {
+        width: 100%;
+        height: auto;
+    }
+    &.notFound {
+        background-color: black;
+    }
+}
+</style>
