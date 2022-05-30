9f3d2e52dbbfd69fb2f7de761fdabe9cab9d329a
Saveliy Graboviy$ git config --global user.name Saveliy <graboviys@yandex.ru>
2022-05-30 19:51:49 +0300

    starting index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
-    <div>
-        <p>
-            <NuxtLink to="/Settings">жжжж гыгыгы</NuxtLink>
-        </p>
-
-        <QuantityCounter v-model:rounds="rounds"></QuantityCounter>
+    <div class="background-wrap">
+        <!-- <QuantityCounter v-model:rounds="rounds"></QuantityCounter>
 
         <div class="buttons">
             <h3>Кнопки</h3>
@@ -20,7 +16,21 @@
             <Slider v-model="slider" :min="0" :max="100" />
         </div>
 
-        {{ slider }}
+        {{ slider }} -->
+        <div class="circle circle-top">
+            <h2>Мы рады видеть вас снова!</h2>
+        </div>
+        <div class="form-wrapper">
+            <h1>Вход</h1>
+            <form action="">
+                <Button @click="lala">Войти</Button>
+            </form>
+            <a href="">Зарегистрироваться</a>
+        </div>
+        <div class="circle circle-bottom">
+            <p class="help-text">Войти с помощью</p>
+            <div class="icons-wrapper"></div>
+        </div>
     </div>
 </template>
 
@@ -41,9 +51,34 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+    overflow: hidden;
+}
+
+h2 {
+    font-weight: bold;
+    font-size: 44px;
+    max-width: 500px;
+    color: white;
+    text-align: center;
+    position: absolute;
+    // top: 300px;
+    bottom: 55px;
+}
+
+.background-wrap {
+    background-color: #00214e;
+    height: 100vh;
+    width: 100vw;
+}
+
+.form-wrap {
+}
+
 .slider-block {
     margin: 30px;
 }
+
 .buttons {
     padding: 10px;
     & h3 {
@@ -55,5 +90,27 @@ export default {
         gap: 10px;
         align-items: flex-start;
     }
+}
+
+.circle-top {
+    position: absolute;
+    transform: translateY(-50%);
+    height: 470px;
+    width: 100vw;
+    background: #ea425c;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+.circle-bottom {
+    position: absolute;
+    transform: translateY(150%);
+    height: 470px;
+    width: 100vw;
+    background: #ffbd00;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
 }
 </style>
