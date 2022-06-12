2cfdf3910ce76fd358698b28240fe84920398206
DaryaSsss <d.semina10@gmail.com>
2022-06-12 19:00:50 +0300

    Sign in Log in with Email+Password, Sign in with Google Account

--- app.vue
+++ app.vue
@@ -2,8 +2,19 @@
   <NuxtPage />
 </template>
 
-<script>
-export default {};
+<script setup>
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCg9ZsvpeikE5w6cGb3Q428MF43TN-7_3Q",
+  authDomain: "quizza-915c1.firebaseapp.com",
+  projectId: "quizza-915c1",
+  storageBucket: "quizza-915c1.appspot.com",
+  messagingSenderId: "670860389883",
+  appId: "1:670860389883:web:db8150a4bc04cf9181ef5e"
+};
+
+const app = initializeApp(firebaseConfig);
 </script>
 
 <style>
